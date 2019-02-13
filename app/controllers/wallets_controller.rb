faf27ac2521f372ca1413d6a86966541edda7025
Darshan A S <itisdarshan@gmail.com>
2019-02-13 13:29:21 +0530

    Add a check for withdrawal of negative amount in the controller
    - Add a unit test case which checks if an bad request response is sent on withdrawal of a negative amount
    - Modify #withdrawal to send bad request response with a JSON error when a withdrawal of negative amount is made

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -14,8 +14,8 @@ class WalletsController < ApplicationController
 		wallet = Wallet.find(params[:id])
 		begin
 			wallet.withdraw(params[:amount].to_f)
-		rescue
-			render json: { error: 'Insufficient Balance' }, status: 400 and return
+		rescue => e
+			render json: { error: e.message }, status: 400 and return
 		end
 		render json: wallet, status: 200
 	end
