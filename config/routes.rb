630eca19de66fddd30ff8111f6e436cfcf2382e4
Darshan A S <itisdarshan@gmail.com>
2019-02-14 16:27:47 +0530

    Enable CORS

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 	# get 'wallets/new' => 'wallets#new'
 	# get 'wallets/:id' => 'wallets#balance'
-	# resources
-	get 'wallets/:id/show' => 'wallets#show'
+
+	get 'wallets/:id' => 'wallets#show'
 	post 'wallets/:id/deposit' => 'wallets#deposit'
 	post 'wallets/:id/withdraw' => 'wallets#withdraw'
+
+	# resources :wallets, only: [:show] do
+	# 	post :deposit
+	# 	post :withdraw
+	# end
 end
